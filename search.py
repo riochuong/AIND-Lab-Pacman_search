6c513be1d9edd07c813c327c502986a96e57d368
chuongdao <cd.kernel.dev.105@gmail.com>
2017-05-31 08:38:46 -0700

    complete astar

--- search.py
+++ search.py
@@ -12,6 +12,7 @@ by Pacman agents (in searchAgents.py).
 """
 
 import util
+from util import PriorityQueue
 
 
 class SearchProblem:
@@ -220,9 +221,125 @@ def breadthFirstSearch(problem):
   util.raiseNotDefined()
 
 
+LOC = 0
+ACTION = 1
+COST = 2
+COME_FROM = 3
+PATH_COST = 4
+
+class Frontier :
+  def __init__(self):
+    self.queue = PriorityQueue()
+    self.ht = {}
+
+  def pop(self):
+    while len(self.ht):
+      val = self.queue.pop()
+      # delete from ht also
+      #print(self.ht)
+      #print("pop:", val, " val loc",val[LOC])
+      
+      # discard item that is not in the dict
+      if (val[LOC] not in self.ht.keys()):
+        continue
+      
+      del self.ht[val[LOC]]
+      # clean up queue 
+      if (not len(self.ht)):
+        while (not self.queue.isEmpty()):
+          self.queue.pop()
+
+      print("pop val", val)
+      return val
+    else:
+      # clean up and pop queue
+      while (not self.queue.isEmpty()):
+        self.queue.pop()
+    return None
+
+  def push(self, item):
+    # push item to queue with priority as cost
+    #print("push", item, "loc ",item[LOC])
+    self.queue.push(item, item[COST])
+    # keep track item cost in frontier 
+    self.ht[item[LOC]] = item[COST]
+
+  def getItemCost(self, key):
+    if (key in self.ht):
+      return self.ht[key]
+
+  def isEmpty(self):
+    return (len(self.ht) == 0)
+
+  # check if key is in frontier 
+  def isInFrontier(self, key):
+    return key in self.ht
+
 def uniformCostSearch(problem):
   "Search the node of least total cost first. "
-  "*** YOUR CODE HERE ***"
+  
+  # this class represent a frontier 
+  # to support member ship check and 
+  # retreived lowest cost path 
+
+
+  # start the project here 
+  
+  initial_state = problem.getStartState()
+  initial_state = (initial_state, None, 0, None)
+  frontiers = Frontier()
+  frontiers.push(initial_state)
+  explored = set()
+  destination = None
+  actions = []
+
+  # main loop 
+  while (not frontiers.isEmpty()):
+    next_frontier = frontiers.pop()
+    #print("Next Frontier", next_frontier)
+
+    # skip to next 
+    if (next_frontier[LOC] in explored):
+      continue
+    # check goal state
+    if (problem.isGoalState(next_frontier[LOC])):
+      #print("found destination", next_frontier[LOC])
+      destination = next_frontier
+      break
+
+    # add to explored
+    explored.add(next_frontier[LOC])
+
+    # update frontiers
+    for new_frontier in problem.getSuccessors(next_frontier[LOC]):
+      # upate cost for the new possible frontier
+      #print("new frontier", new_frontier)
+      new_cost = new_frontier[COST] + next_frontier[COST]
+      # hacky way to update the cost
+      convert_obj = list(new_frontier)
+      convert_obj[COST] = new_cost
+      new_frontier = tuple(convert_obj)
+      # update to know where they come from 
+      new_frontier += (next_frontier,)
+      if (not new_frontier[LOC] in explored) and (not frontiers.isInFrontier(new_frontier[LOC])):
+        frontiers.push(new_frontier)
+      # check if we found a shorter path 
+      elif frontiers.isInFrontier(new_frontier[LOC]) \
+        and (new_frontier[COST] < frontiers.getItemCost(new_frontier[LOC])):
+        # update the frontier in the tree 
+        frontiers.push(new_frontier)
+
+  # now reconstruct the path 
+  # back track the path
+  if destination:
+    print("Destination", destination)
+    while destination[COME_FROM]:
+      actions.append(destination[ACTION])
+      destination = destination[COME_FROM]
+    actions = [act for act in reversed(actions)]
+  
+  return actions
+
   util.raiseNotDefined()
 
 
@@ -237,6 +354,74 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
   "Search the node that has the lowest combined cost and heuristic first."
   "*** YOUR CODE HERE ***"
+  "Search the node of least total cost first. "
+  
+  # this class represent a frontier 
+  # to support member ship check and 
+  # retreived lowest cost path 
+
+
+  # start the project here 
+  
+  initial_state = problem.getStartState()
+  initial_state = (initial_state, None, heuristic(initial_state,problem), None,0)
+  #print('initial state', initial_state)
+  frontiers = Frontier()
+  frontiers.push(initial_state)
+  explored = set()
+  destination = None
+  actions = []
+
+  # main loop 
+  while (not frontiers.isEmpty()):
+    next_frontier = frontiers.pop()
+    #print("Next Frontier", next_frontier)
+
+    # skip to next 
+    if (next_frontier[LOC] in explored):
+      continue
+    # check goal state
+    if (problem.isGoalState(next_frontier[LOC])):
+      #print("found destination", next_frontier[LOC])
+      destination = next_frontier
+      break
+
+    # add to explored
+    explored.add(next_frontier[LOC])
+
+    # update frontiers
+    for new_frontier in problem.getSuccessors(next_frontier[LOC]):
+      # upate cost for the new possible frontier
+      h_val = heuristic(new_frontier[LOC], problem)
+      new_path_cost = new_frontier[COST] + next_frontier[PATH_COST] 
+      new_cost = new_path_cost + h_val
+      # hacky way to update the cost
+      convert_obj = list(new_frontier)
+      convert_obj[COST] = new_cost
+      new_frontier = tuple(convert_obj)
+      # update to know where they come from 
+      new_frontier += (next_frontier,)
+      new_frontier += (new_path_cost,)
+      if (not new_frontier[LOC] in explored) and (not frontiers.isInFrontier(new_frontier[LOC])):
+        frontiers.push(new_frontier)
+      # check if we found a shorter path 
+      elif frontiers.isInFrontier(new_frontier[LOC]) \
+        and (new_frontier[COST] < frontiers.getItemCost(new_frontier[LOC])):
+        # update the frontier in the tree 
+        frontiers.push(new_frontier)
+
+  # now reconstruct the path 
+  # back track the path
+  if destination:
+    print("Destination", destination)
+    while destination[COME_FROM]:
+      actions.append(destination[ACTION])
+      destination = destination[COME_FROM]
+    actions = [act for act in reversed(actions)]
+  
+  return actions
+
+  util.raiseNotDefined()
   util.raiseNotDefined()
 
 
